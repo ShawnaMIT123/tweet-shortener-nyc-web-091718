4e71957f68ae11dfb575c235c25dde61a5d1073a
Shawna Davis <shawna_davis@alum.mit.edu>
2018-09-14 19:42:13 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -5,13 +5,14 @@ dictionary = {
 "two"=> "2", 
 "too" => "2",
 "for"=> "4",
-four"=> '4',
-'be' => 'b',
-'you' =>'u',
+"four"=> "4",
+"be" => "b",
+"you" =>"u",
 "at" => "@" ,
 "and" => "&"}
 end
 
 def word_substituter(string)
+  
 end
  
